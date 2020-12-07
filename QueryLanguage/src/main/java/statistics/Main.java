86c9a3dbc345d11774b96c33266bc2e8039a884c
zeeket <3qvm@tutanota.com>
2020-12-07 17:00:02 +0200

    get assignment code

---
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -0,0 +1,20 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
+
+        Statistics stats = new Statistics(new PlayerReaderImpl(url));
+          
+        Matcher m = new And( new HasAtLeast(5, "goals"),
+                             new HasAtLeast(5, "assists"),
+                             new PlaysIn("PHI")
+        );
+        
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
+    }
+}
